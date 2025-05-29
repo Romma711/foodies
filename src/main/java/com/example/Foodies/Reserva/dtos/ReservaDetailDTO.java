27d8b93687a8af774f639cacdf76c8175df2c3bc
juanestavillo <palilloestavillo@gmail.com>
2025-05-29 11:11:48 -0300

    DTO resena y reserva

---
+++ src/main/java/com/example/Foodies/Reserva/dtos/ReservaDetailDTO.java
@@ -0,0 +1,15 @@
+package com.example.Foodies.Reserva.dtos;
+
+import com.example.Foodies.Enums.EstadoReserva;
+
+import java.time.LocalTime;
+
+public record ReservaDetailDTO(
+    Long id,
+    Long cantidad,
+    LocalTime horariollegada,
+    LocalTime horariofin,
+    EstadoReserva estadoReserva,
+    Long idCliente,
+    Long idRestaurant
+) { }
